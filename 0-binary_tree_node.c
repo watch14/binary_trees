80208f2080f8cf4a32b584af7c6a6a58a216d60b
watch14 <watchman1414@gmail.com>
2024-01-02 16:07:28 +0000

    fast_push

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,26 @@
 #include "binary_trees.h"
-#include <stdlib.h>
+
+/**
+ * binary_tree_node - Creates a new binary tree node
+ * @parent: Pointer to the parent node
+ * @value: Integer value to be stored in the new node
+ *
+ * Return: Pointer to the new node, or NULL on failure
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	return;
+	binary_tree_t new_br;
+
+	new_br = malloc(sizeof(binary_tree_t));
+
+	if (!new_br)
+		return (NULL);
+
+	new_br->n = value;
+	new_br->parent = parent;
+	new_br->right = NULL;
+	new_br->left = NULL;
+
+	return (new_br);
 }
