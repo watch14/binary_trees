baad7b01dfb02c72ee485236b5598308108e263f
watch14 <watchman1414@gmail.com>
2024-01-02 16:23:21 +0000

    fast_push

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,7 +10,7 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t new_br;
+	binary_tree_t *new_br;
 
 	new_br = malloc(sizeof(binary_tree_t));
 
---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Inserts a new node to the left
+ * @parent: Pointer to the parent node
+ * @value: Integer value to be stored in the new node
+ *
+ * Return: Pointer to the new node, or NULL on failure
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_br;
+
+	if (!parent)
+		return (NULL);
+
+	new_br = binary_tree_node(parent, value);
+
+	if (!new_br)
+		return (NULL);
+
+	if (parent->left)
+	{
+		new_br->left = parent->left;
+		parent->left->parent = new_br;
+	}
+	parent->left = new_br;
+	return (new_br);
+}
