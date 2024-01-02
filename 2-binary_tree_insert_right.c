46be82253aee5be1813c943dee1d4c3a17a065d0
watch14 <watchman1414@gmail.com>
2024-01-02 16:39:46 +0000

    fast_push

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Inserts a new node to the right
+ * @parent: Pointer to the parent node
+ * @value: Integer value to be stored in the new node
+ *
+ * Return: Pointer to the new node, or NULL on failure
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t new_br;
+
+	if (!parent)
+		return (NULL);
+
+	new_br = binary_tree_node(parent, value);
+
+	if (!new_br)
+		return (NULL);
+
+	if (parent->right)
+	{
+		parent->right = new_br->right;
+		parent->right->parent = new_br;
+	}
+	parent->left = new_br;
+	return (new_br);
+}
