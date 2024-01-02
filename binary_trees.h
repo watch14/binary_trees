fc025ac085198bdd52ea04b5dd9f368f1884ea8d
watch14 <watchman1414@gmail.com>
2024-01-02 13:04:46 +0000

    fast_push

--- binary_trees.h
+++ binary_trees.h
@@ -11,10 +11,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 /*main funcyiont ---> Basic Binary Tree <-- */
 typedef struct binary_tree_s binary_tree_t;
