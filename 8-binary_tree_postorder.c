2964568ca6282e86514f793a861cd7fc0c6d2d7e
watch14 <watchman1414@gmail.com>
2024-01-03 14:30:56 +0000

    fast_push

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,7 +11,10 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree || !func)
 		return;
 
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
+	else
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+	}
 	func(tree->n);
 }
