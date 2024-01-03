0f9fdde47484677daa91173763f3eb9229f6ea9a
watch14 <watchman1414@gmail.com>
2024-01-03 14:48:29 +0000

    fast_push

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,8 +14,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	lh = binary_tree_height(tree->left);
-	rh = binary_tree_height(tree->right);
+	lh = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	rh = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return ((lh > rh) ? lh : rh);
 
-	return (lh > rh ? lh + 1 : rh + 1);
 }
