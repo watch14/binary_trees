ad3c48da5670cd9e12076cfe8b81233a04bd90fd
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-03 14:28:02 +0000

    leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,11 +8,11 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-        return (0);
+	size_t left_leaves = binary_tree_leaves(tree->left);
+	size_t right_leaves = binary_tree_leaves(tree->right);
 
-    size_t left_leaves = binary_tree_leaves(tree->left);
-    size_t right_leaves = binary_tree_leaves(tree->right);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
 
-    return (left_leaves + right_leaves + 1);
+	return (left_leaves + right_leaves + 1);
 }
