b1eb42af2b3e16a91014d266a93a6c74d8dc33ca
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-03 14:50:37 +0000

    Library

--- binary_trees.h
+++ binary_trees.h
@@ -28,30 +28,64 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/*functions*/
+/* functions */
+
+/* Task 0 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* Task 1 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/* Task 2 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/* Task 3 */
 void binary_tree_delete(binary_tree_t *tree);
+
+/* Task 4 */
 int binary_tree_is_leaf(const binary_tree_t *node);
+
+/* Task 5 */
 int binary_tree_is_root(const binary_tree_t *node);
+
+/* Task 6 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 7 */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 8 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 9 */
 size_t binary_tree_height(const binary_tree_t *tree);
+
+/* Task 10 */
 size_t binary_tree_depth(const binary_tree_t *tree);
+
+/* Task 11 */
 size_t binary_tree_size(const binary_tree_t *tree);
+
+/* Task 12 */
 size_t binary_tree_leaves(const binary_tree_t *tree);
+
+/* Task 13 */
 size_t binary_tree_nodes(const binary_tree_t *tree);
+
+/* Task 14 */
 int binary_tree_balance(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/* Task 15 */
 int binary_tree_is_full(const binary_tree_t *tree);
+
+/* Task 16 */
 int binary_tree_is_perfect(const binary_tree_t *tree);
+
+/* Task 17 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/* Task 18 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
-
-
-
-
 
 #endif
