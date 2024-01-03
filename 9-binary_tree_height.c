8a9eac3cef264cfbd380f67a9bb5c97f9e190d23
watch14 <watchman1414@gmail.com>
2024-01-03 14:41:10 +0000

    fast_push

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Calculates the height of a binary tree
+ * @tree: Pointer to the root of the binary tree
+ *
+ * Return: Height of the binary tree, or 0 if tree is NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lh, rh;
+
+	if (!tree)
+		return (0);
+
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
+
+	return (lh > rh ? lh + 1 : rh + 1);
+}
