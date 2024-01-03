f2d0dca1799bd114340a104d1a22b49ce6479b7d
watch14 <watchman1414@gmail.com>
2024-01-03 14:32:41 +0000

    fast_push

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -4,16 +4,13 @@
  * binary_tree_postorder - Performs a post-order traversal on a binary tree
  * @tree: Pointer to the root of the binary tree
  * @func: Pointer to a function to apply to each node's value
-*/
+ */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
 
-	else
-	{
-		binary_tree_postorder(tree->left, func);
-		binary_tree_postorder(tree->right, func);
-	}
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
