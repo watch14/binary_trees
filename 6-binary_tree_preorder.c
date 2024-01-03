b29bba0615fc2bdee8df7830213f697bfbd14a81
watch14 <watchman1414@gmail.com>
2024-01-03 13:56:05 +0000

    fast_push

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,10 +1,14 @@
 #include "binary_trees.h"
 
-
+/**
+ * binary_tree_preorder - Performs a pre-order traversal on a binary tree
+ * @tree: Pointer to the root of the binary tree
+ * @func: Pointer to a function to apply to each node's value
+ */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !fucn)
+	if (!tree || !func)
 		return;
 
 	func(tree->n);
