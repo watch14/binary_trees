ce2399c98298bf4dda1abce503591062e41da349
watch14 <watchman1414@gmail.com>
2024-01-03 13:27:58 +0000

    fast_push

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,12 +1,17 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root of the binary tree
+ */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	binary_tree_t br;
-
 	if (!tree)
 		return (NULL);
 
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 
+	free(tree);
 }
