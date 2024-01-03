281ae9d7897f774c873a8b2ff380de7edee13836
watch14 <watchman1414@gmail.com>
2024-01-03 12:37:55 +0000

    fast_push

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -22,7 +22,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (parent->right)
 	{
-		parent->right = new_br->right;
+		new_br->right = parent->right;
 		parent->right->parent = new_br;
 	}
 	parent->right = new_br;
